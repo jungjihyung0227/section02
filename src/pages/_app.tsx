428d437ed7d9c248631802ad14a9b04b1fe421a1
정지형 <gw03176370@gmail.com>
2025-03-29 18:55:01 +0900

    feat : ssg 작동 방식 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
 import GlobalLayout from "@/component/global-layout";
-import SearchableLayout from "@/component/searchble-layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { ReactNode } from "react";
 
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
 // 모든 페이지에 사용되기 때문에 글로벌 css이 사용 가능함.
-export default function App({ Component, pageProps }: AppProps) {
-  const getLayout = Component.getLayout;
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & { Component: NextPageWithLayout }) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
   return (
     <>
-      <GlobalLayout>
-        <SearchableLayout>
-          {getLayout(<Component {...pageProps} />)}
-        </SearchableLayout>
-      </GlobalLayout>
+      <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>
     </>
   );
 }
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,37 @@
+import SearchableLayout from "@/component/searchble-layout";
 import { useRouter } from "next/router";
 
+import BookItem from "@/component/book-item";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+  return {
+    props: {
+      books,
+    },
+  };
+};
 // 페이지 라우팅
-export default function Page() {
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
   console.log(router.query);
-  return <h1>setting</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 // 쿼리스트링이 바뀔때마다 렌더링함.
+
+Page.getLayout = (page: React.ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
